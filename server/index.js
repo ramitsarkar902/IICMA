a15d018b0f4e974aa008c524c70528d94ef3a798
Ramit Sarkar <ramitsarkar@Ramits-MacBook-Air.local>
2023-01-21 19:41:37 +0530

    fixing depolyment issues

--- server/index.js
+++ server/index.js
@@ -14,6 +14,7 @@ import userRoutes from "./routes/user.js";
 dotenv.config();
 const app = express();
 
+mongoose.set("strictQuery", false);
 const connect = () => {
   mongoose
     .connect(process.env.MONGO_URL)
