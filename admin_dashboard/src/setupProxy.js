e4c9595392947a58047ae656d0b712c95c64c848
Ramit Sarkar <ramitsarkar@Ramits-Air.mshome.net>
2023-02-01 17:22:51 +0530

    vercel try

--- admin_dashboard/src/setupProxy.js
+++ admin_dashboard/src/setupProxy.js
@@ -1,4 +1,4 @@
-const { createProxyMiddleware } = require('http-proxy-middleware');
+/* const { createProxyMiddleware } = require('http-proxy-middleware');
 module.exports = function(app) {
   app.use(
     '/api',
@@ -7,4 +7,4 @@ module.exports = function(app) {
       changeOrigin: true,
     })
   );
-};
+}; */
